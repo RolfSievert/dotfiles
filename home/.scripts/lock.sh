6d4388d699645c1b39146aaab7bd2cfb9c4c950b
RolfSievert <26115145+RolfSievert@users.noreply.github.com>
2019-02-11 11:54:02 +0100

    Fixed lock colors read from wal colors2

--- home/.scripts/lock.sh
+++ home/.scripts/lock.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 
-B='#00000000'  # blank
-C='#ffffff22'  # clear ish
-D='#ff00ffcc'  # default
-T='#ee00eeee'  # text
-W='#880000bb'  # wrong
-V='#bb00bbbb'  # verifying
+# Path to wal colors: .cache/wal/colors.Xresources
+
+#B="color0"  # blank
+#C='#ffffff22'  # clear ish
+#D='#ff00ffcc'  # default
+#T='#ee00eeee'  # text
+#W='#880000bb'  # wrong
+#V='#bb00bbbb'  # verifying
+
+#Colors path
+COL_PATH=/home/.cache/wal/colors.Xresources
+B=$(echo $(awk '/\*color0:(.*)/ { print $2 }' < ~/.cache/wal/colors.Xresources)cc)
+C=$(echo $(awk '/\*color0:(.*)/ { print $2 }' < ~/.cache/wal/colors.Xresources)ff)
+D=$(echo $(awk '/\*color2:(.*)/ { print $2 }' < ~/.cache/wal/colors.Xresources)ff)
+T=$(echo $(awk '/\*color4:(.*)/ { print $2 }' < ~/.cache/wal/colors.Xresources)ff)
+W=$(echo $(awk '/\*color5:(.*)/ { print $2 }' < ~/.cache/wal/colors.Xresources)ff)
+V=$(echo $(awk '/\*color3:(.*)/ { print $2 }' < ~/.cache/wal/colors.Xresources)ff)
+
 
 i3lock \
 --insidevercolor=$C   \
