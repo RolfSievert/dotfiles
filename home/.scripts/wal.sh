dffb29424c7e47f7302ec601a3683f7fbdff3bd4
RolfSievert <26115145+RolfSievert@users.noreply.github.com>
2019-02-16 20:21:42 +0100

    Send image as argument

--- home/.scripts/wal.sh
+++ home/.scripts/wal.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-I=~/Pictures/5vvyo4u8y8wz.jpg # image (w h ratio = 1.78)
+#I=~/Pictures/5vvyo4u8y8wz.jpg # image (w h ratio = 1.78)
 S=0.35 # saturation
 
-wal -g -i $I --saturate $S --backend haishoku
+wal -i $1 --saturate $S --backend haishoku
 
 # Backends:
 # colorz, schemer2, haishoku, colorthief, wal
