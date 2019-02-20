80494f89f31539302634db3afb3417ab0ae323cb
RolfSievert <26115145+RolfSievert@users.noreply.github.com>
2019-02-20 13:21:45 +0100

    Replaced modules in polybar

--- home/.config/polybar/spotify.sh
+++ home/.config/polybar/spotify.sh
@@ -3,7 +3,7 @@ status=`exec playerctl status 2> /dev/null`
 if [ "$status" = "Playing" ]; then
   title=`exec playerctl metadata xesam:title`
   artist=`exec playerctl metadata xesam:artist`
-  echo " $title - $artist"
+  echo "$title - $artist"
 elif [ "$status" = "Paused" ]; then
-  echo ""
+  echo ""
 fi
