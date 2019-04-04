d6cb225f489498ac78251fa4eb7bd57829a48097
RolfSievert <26115145+RolfSievert@users.noreply.github.com>
2019-04-04 04:26:30 +0200

    Added list AURs and moved folders

--- home/.scripts/maintenance/clean_system.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo Removing unused orphans...
-if ! [ -z "$(pakku -Qtdq)"]; then
-    pakku -Rns $(pakku -Qtdq)
-    echo Done.
-else 
-    echo No unused orphans exist.
-fi
--- home/.scripts/remount_tmp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo mount -o remount,size=2500M,noatime /tmp
-echo "Done. Please use 'df -h' to make sure folder size is increased."
