e7bd0fe59aaea5e211513d56a8d1778d313a2726
RolfSievert <26115145+RolfSievert@users.noreply.github.com>
2019-08-14 13:36:36 +0200

    Code comment

--- home/.scripts/system/enable-services.sh
+++ home/.scripts/system/enable-services.sh
@@ -4,6 +4,7 @@
 
 sudo true
 
+# Create dir if nonexisting
 if [ ! -d "~/.config/systemd/user" ]; then
     mkdir -p ~/.config/systemd/user
 fi
@@ -12,10 +13,11 @@ for filename in $(dirname $0)/services/*.service; do
     # is user service
     if grep -q "WantedBy=default.target" "$(realpath $filename)"; then
         echo "Creating user service $(basename $filename .service)"
+        # Create user service, overwrite if existing, and start the service now
         sudo systemctl --user -f enable $(realpath $filename) --now
     else # system-wide service
         # Sevice name
-        sudo systemctl enable "$(realpath $filename)" --now
+        sudo systemctl enable -f "$(realpath $filename)" --now
     fi
     echo start
 done
