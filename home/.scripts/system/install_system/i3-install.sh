f6757bb11bc7b0f3ea06b1d895a18c8c9f473609
Rolf Sievert <26115145+RolfSievert@users.noreply.github.com>
2019-10-14 19:05:15 +0200

    Updated installer

--- home/.scripts/system/install_system/i3-install.sh
+++ home/.scripts/system/install_system/i3-install.sh
@@ -2,15 +2,21 @@
 
 # Install general packages
 
-# Install i3
+# (Install i3)
 
 # Install i3 packages
 
-# Copy services
-cp ~/.scripts/system/services/* ~/.config/systemd/user
+# Check for an ssh key, else generate and open github afterwards and wait for user input
 
-# Enable services
-systemctl --user enable battery-monitor.service
-systemctl --user enable lock@hitsnapper.service
-systemctl --user start battery-monitor.service
-systemctl --user start lock@hitsnapper.service
+# Check if castle dotfiles is initiated, else clone and link
+
+# Install oh my zsh
+
+# Install Plug for nvim
+
+# Link vimrc to .config/nvim/init.vim
+
+# Install YouCompleteMe
+
+# Start services
+
