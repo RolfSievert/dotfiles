54dbdca8b6b955913a010755a1d8cdcf18c7dae0
Rolf Sievert <26115145+RolfSievert@users.noreply.github.com>
2019-10-17 12:38:19 +0200

    Removed old script

--- home/.scripts/colorscheme/change-colorscheme.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-#
-# change-colorscheme.sh
-# Copyright (C) 2019 hitsnapper <hitsnapper@Znap>
-#
-# Distributed under terms of the MIT license.
-#
-
-rofi -show Colorscheme -modi Colorscheme:~/.scripts/colorscheme/colorscheme-selector.sh
