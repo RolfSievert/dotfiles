7f5ea15d64518b4b8bd009353d61bfef0a25ab34
Rolf Sievert <26115145+RolfSievert@users.noreply.github.com>
2019-10-18 21:55:52 +0200

    Updated installer script

--- home/.scripts/system/install_system/i3-install.sh
+++ home/.scripts/system/install_system/i3-install.sh
@@ -1,22 +1,55 @@
 #! /bin/sh
 
 # Install general packages
+PACKAGES = $(cat ~/.scripts/system/install_system/general_packages.txt | grep -v '#')
+yay -S --needed $PACKAGES
 
 # (Install i3)
 
 # Install i3 packages
+PACKAGES = $(cat ~/.scripts/system/install_system/i3_packages.txt | grep -v '#')
+yay -S --needed $PACKAGES
+
+# Run wal
 
 # Check for an ssh key, else generate and open github afterwards and wait for user input
+read -p "Do you want to crete a ssh key? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    # do dangerous stuff
+fi
 
 # Check if castle dotfiles is initiated, else clone and link
 
 # Install oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Build nvim with python for YCM support
+pip3 install --user neovim
 
 # Install Plug for nvim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Link vimrc to .config/nvim/init.vim
+if [ ! -d ~/.config/nvim ]; then
+    mkdir ~/.config/nvim;
+fi
+ln ~/.vimrc ~/.config/nvim/init.vim
 
 # Install YouCompleteMe
+cd ~/.vim/plugged/YouCompleteMe/
+sudo python3 ./install.py
 
 # Start services
+cd
+sudo ~/.scripts/system/enable-services.sh
 
+# Set colorscheme with wal
+read -p "Do you wish to set a colorscheme from wal folder? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    # do dangerous stuff
+fi
