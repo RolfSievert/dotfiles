c51695e7789e0437625ebd0842f9bf35e05afc78
Rolf Sievert <26115145+RolfSievert@users.noreply.github.com>
2020-01-24 11:30:24 +0000

    Improved clean system script

--- home/.scripts/system/clean-system.sh
+++ home/.scripts/system/clean-system.sh
@@ -1,26 +1,60 @@
 #!/bin/sh
-echo Removing unused orphans...
-if ! [ -z "$(pakku -Qtdq)"]; then
-    pakku -Rns $(pakku -Qtdq)
-    echo Done.
-else 
-    echo No unused orphans exist.
+sudo true
+
+# Size of package including dependencies not used by other packages
+package_size() {
+    pacman -Rs --print-format '%s' $1 | awk '{size+=$1} END {print size/1048576}'
+}
+sum_package_sizes() {
+    SIZE=0
+    for var in "$@"; do
+        SIZE=$SIZE+$(package_size $var)
+    done
+    echo $SIZE
+}
+
+if ! [ -z "$(pacman -Qtdq)"]; then
+    # -n flag: do not save important configuration files
+    echo Removing unused orphans...
+    sudo pacman -Rns $(pacman -Qtdq)
 fi
 
-# Remove cached packages
-echo Do you want to remove cached packages? This will make installment of packages offline impossible.
-pakku -Sc
-
 # Clear journal
-echo Removing logs...
+echo Removing old logs...
 sudo journalctl --vacuum-time=7d
 
 # Remove broken symlinks
-echo $(sudo find / -type d -path /proc -prune -o -xtype l -print)
+BROKEN_SYMLINKS=$(sudo find '/' -type d \( -path '/proc' -o -path '/run' \) -prune -o -xtype l)
 
-echo Do you want to remove the broken symlinks listed above?
+if [[ -z "${BROKEN_SYMLINKS// }" ]]; then
+    printf '%s\n' "${BROKEN_SYMLINKS[@]}"
 
-echo NOTE REMOVED SYMLINKS
+    SYMSIZE=`sudo du -sm ${BROKEN_SYMLINKS[@]}`
+    read -p "Do you want to remove the broken symlinks listed above? ($SYMSIZE) " -r
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        sudo rm -f ${BROKEN_SYMLINKS[@]}
+    fi
+fi
+
+# Stores result in $REPLY
+read -p "Do you want to remove uninstalled cached packages? " -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    # Remove not installed cached packages
+    # The extra 'c' ensures nothing is left in cache
+    sudo pacman -Scc
+fi
 
 # Remove cashed packages except the most recent ones
-paccache -rk1
+# removal candidates: paccache -dk1
+read -p "Do you want to remove cached packages (except the latest versions)? " -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    sudo paccache -rk1
+fi
+paccache -dk1
+CACHE_SPACE=`sudo du -sh ~/.cache/`
+echo ~/.cache/ occupies $CACHE_SPACE
+read -p "Do you wish to clean cache? " -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    # Remove not installed cashed packages
+    sudo rm -rf ~/.cache/
+fi
---
+++ home/.scripts/system/install_system/create_swap.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#
+# create_swap.sh
+# Copyright (C) 2019 rolfsievert <rolfsievert@manjaro>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+
