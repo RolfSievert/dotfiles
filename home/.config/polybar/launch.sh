d958f4a941e5ee34f9aa41cc740ffe33ab602124
Rolf <26115145+RolfSievert@users.noreply.github.com>
2022-06-05 15:54:53 +0200

    fixed polybar launch from i3

--- home/.config/polybar/launch.sh
+++ home/.config/polybar/launch.sh
@@ -2,9 +2,9 @@
 
 # Terminate already running bar instances
 # If all your bars have ipc enabled, you can use
-polybar-msg cmd quit
+# polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
-# killall -q polybar
+killall -q polybar; sleep 1
 
 # Wait until the processes have been shut down
 #while pgrep -x polybar > ~/.config/polybar/launch_log.txt; do sleep 1; done
