fe3a3da919e3e906ee983ba02bc87cb40d2c8162
Rolf <26115145+RolfSievert@users.noreply.github.com>
2022-10-31 11:29:55 +0100

    fixed volume mute not displaying correctly

--- home/.config/i3/volume.sh
+++ home/.config/i3/volume.sh
@@ -11,25 +11,23 @@ source $(dirname $0)/message_bar.sh
 msgId="991049"
 
 function get_volume {
-    pactl get-sink-volume $(get_default_sink) | \
-        head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,'
+    # exclude second match of front-right with extra %
+    pactl get-sink-volume "$1" | grep -P -o '[0-9]*(?=%.*%)'
 }
 
 function is_mute {
-    pacmd list-sinks | awk '/muted/ { print $2 }'
+    pactl get-sink-mute "$1" | awk '/Mute: / { print $2 }'
 }
 
 function get_default_sink {
     echo "$(pactl get-default-sink)"
 }
 
-# Difference to change volume with
-vdiff=$2
 function send_notification {
-    volume=`get_volume`
+    volume=$(get_volume $1)
 
     # Set title
-    if [[ "$(is_mute)" == *"yes"* ]]; then
+    if [[ "$(is_mute $1)" == *"yes"* ]]; then
         title="Volume Muted"
     else
         title="Volume"
@@ -39,24 +37,26 @@ function send_notification {
 }
 
 sink=`get_default_sink`
+vdiff=$2 # Difference to change volume with
+
 case $1 in
     up)
         # Up the volume
-        volume=`get_volume`
-        if [[ $volume -ge 95 ]]; then
+        volume=$(get_volume $sink)
+        if [[ "$volume" -ge 95 ]]; then
             pactl set-sink-volume "$sink" 100%
         else
             pactl set-sink-volume "$sink" +$vdiff%
         fi
-        send_notification
+        send_notification $sink
         ;;
     down)
         pactl set-sink-volume "$sink" -$vdiff%
-	    send_notification
+	    send_notification $sink
 	    ;;
     mute)
     	# Toggle mute
         pactl set-sink-mute "$sink" toggle
-        send_notification
+        send_notification $sink
 	    ;;
 esac
