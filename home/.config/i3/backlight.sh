7ffa845e2db4a4b687946e0b27e191b2ff3b004f
Rolf Sievert <26115145+RolfSievert@users.noreply.github.com>
2023-07-15 00:24:56 +0200

    fixed laptop backlight

--- home/.config/i3/backlight.sh
+++ home/.config/i3/backlight.sh
@@ -8,35 +8,35 @@ source $(dirname $0)/message_bar.sh
 # Arbitrary but unique message id
 msgId="991050"
 
-function get_brightness {
-    float=`xbacklight -get`
+function get_brightness_percentage {
+    current=$(brightnessctl get)
+    max=$(brightnessctl max)
+    float=$((100 * current / max))
     echo ${float%.*}
 }
 
 # Difference to change volume with
 function send_notification {
-    brightness=`get_brightness`
+    brightness=$(get_brightness_percentage)
 
     # Send the notification
     send_notification_message_bar $brightness "Brightness" $msgId
 }
 
 if [[ "$1" == "up" ]]; then
-    xbacklight -inc "$2"
+    brightnessctl set +$2%
 elif [[ "$1" == "down" ]]; then
-    xbacklight -dec "$2"
+    brightnessctl set $2%-
 elif [[ "$1" == "bright" ]]; then
-    display=`xrandr | grep -w connected | cut -f '1' -d ' '`
+    # simulate increased brightness
+    display=$(xrandr | grep -w connected | cut -f '1' -d ' ')
     xrandr --output $display --brightness "$2"
 else
     echo warning: input not recognized
-    echo usage: 
+    echo usage:
     echo -e "\tup, down - increase/decrease brightness"
     echo -e "\tbright - set percieved screen brightness"
     exit
 fi
 
 send_notification
-
-# TODO use following to fake brightness above 100%
-# xrandr --output output_name --brightness 2
