eea50a901ac14e861991dc5096749ff2dcc91f93
Rolf Sievert <26115145+RolfSievert@users.noreply.github.com>
2024-09-27 14:03:08 +0200

    formatting comment

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,8 +1,9 @@
 -- Tip: run :checkhealth
 
--- TODOs
--- * Assign random colors to the bar containing the current file-name to help separate windows (statusline)
--- * Add grammar check in latex and md (already exists?)
+--[[ TODOs
+ * Assign random colors to the bar containing the current file-name to help separate windows (statusline)
+ * Add grammar check in latex and md (already exists?)
+--]]
 
 require('mappings')
 require('options')
