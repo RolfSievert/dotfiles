016c4a9e8795804b0cf901e671963255765f9935
Rolf Sievert <26115145+RolfSievert@users.noreply.github.com>
2025-06-19 20:00:48 +0200

    fix avante deprecation warnings

--- home/.config/nvim/lua/plugin_configs/avante_options.lua
+++ home/.config/nvim/lua/plugin_configs/avante_options.lua
@@ -1,12 +1,16 @@
 local options = {
   provider = "openai", -- NOTE: you own the code and can use it commercially, but check your region's copyright laws using AI
-  openai = {
-    endpoint = "https://api.openai.com/v1",
-    model = "o4-mini",            -- your desired model (or use gpt-4o, etc.)
-    timeout = 30000,              -- Timeout in milliseconds, increase this for reasoning models
-    temperature = 0,
-    max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-    --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+  providers = {
+    openai = {
+      endpoint = "https://api.openai.com/v1",
+      model = "o4-mini", -- your desired model (or use gpt-4o, etc.)
+      timeout = 30000,   -- Timeout in milliseconds, increase this for reasoning models
+      extra_request_body = {
+        temperature = 0,
+        max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+        --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+      }
+    }
   }
 }
 
