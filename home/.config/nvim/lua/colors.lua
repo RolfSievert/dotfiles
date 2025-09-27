2d4af90eb884859aaae55056afc97dc5efa59d15
Rolf Sievert <26115145+RolfSievert@users.noreply.github.com>
2025-09-27 23:33:21 +0200

    improved coloring of results in telescope

--- home/.config/nvim/lua/colors.lua
+++ home/.config/nvim/lua/colors.lua
@@ -31,15 +31,15 @@ local set_hl = vim.api.nvim_set_hl
 
 ---- Telescope
 -- borders
-set_hl(0, "TelescopePromptBorder", { link = "TelescopeSelection" })
-set_hl(0, "TelescopePreviewBorder", { link = "TelescopeSelection" })
-set_hl(0, "TelescopeResultsBorder", { link = "TelescopeSelection" })
+set_hl(0, "TelescopePromptBorder", { link = "CursorLine" })
+set_hl(0, "TelescopePreviewBorder", { link = "CursorLine" })
+set_hl(0, "TelescopeResultsBorder", { link = "CursorLine" })
 -- prompt
 set_hl(0, "TelescopePromptTitle",
   { bg = get_hl_fg("TelescopePromptTitle"), fg = get_hl_bg("TelescopeNormal"), bold = true })
 set_hl(0, "TelescopePromptPrefix",
-  { fg = get_hl_fg("TelescopePromptPrefix"), bg = get_hl_bg("TelescopeSelection"), bold = true })
-set_hl(0, "TelescopePromptNormal", { link = "TelescopeSelection" })
+  { fg = get_hl_fg("TelescopePromptPrefix"), bg = get_hl_bg("CursorLine"), bold = true })
+set_hl(0, "TelescopePromptNormal", { link = "CursorLine" })
 -- preview
 set_hl(0, "TelescopePreviewTitle",
   { bg = get_hl_fg("TelescopePreviewTitle"), fg = get_hl_bg("TelescopeNormal"), bold = true })
@@ -47,4 +47,9 @@ set_hl(0, "TelescopePreviewNormal", { link = "Normal" })
 -- results
 set_hl(0, "TelescopeResultsTitle",
   { bg = get_hl_fg("TelescopeResultsTitle"), fg = get_hl_bg("TelescopeNormal"), bold = true })
+set_hl(0, "TelescopeResultsNormal",
+  { link = "CursorLine" })
+set_hl(0, "TelescopeSelection", { bg = get_hl_bg("TelescopeNormal") })
+set_hl(0, "TelescopeSelectionCaret",
+  { bg = get_hl_bg("TelescopeNormal"), fg = get_hl_fg("TelescopeSelectionCaret"), bold = true })
 ----
