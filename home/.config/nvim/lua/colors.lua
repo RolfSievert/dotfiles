d5599da4860bfe04ec8cafab37c1d6e2c574f41a
Rolf Sievert <26115145+RolfSievert@users.noreply.github.com>
2025-09-28 11:47:17 +0200

    made colors more easily customized

--- home/.config/nvim/lua/colors.lua
+++ home/.config/nvim/lua/colors.lua
@@ -26,30 +26,90 @@ local function get_bg(hl_name)
   return string.format("#%06x", hl.bg)
 end
 
+-- update this table to set custom color values
+local hightlights = {
+  {
+    name = "TelescopePromptBorder",
+    opts = { link = "CursorLine" }
+  },
+  {
+    name = "TelescopePreviewBorder",
+    opts = { link = "CursorLine" }
+  },
+  {
+    name = "TelescopeResultsBorder",
+    opts = { link = "CursorLine" }
+  },
+  -- prompt
+  {
+    name = "TelescopePromptTitle",
+    opts = {
+      bg = get_fg("TelescopePromptTitle"),
+      fg = get_bg("TelescopeNormal"),
+      bold = true
+    }
+  },
+  {
+    name = "TelescopePromptPrefix",
+    opts = {
+      fg = get_fg("TelescopePromptPrefix"),
+      bg = get_bg("CursorLine"),
+      bold = true
+    }
+  },
+  {
+    name = "TelescopePromptNormal",
+    opts = { link = "CursorLine" }
+  }, -- for some reason this does not affect the prompt text?
+  -- preview
+  {
+    name = "TelescopePreviewTitle",
+    opts = {
+      bg = get_fg("TelescopePreviewTitle"),
+      fg = get_bg("TelescopeNormal"),
+      bold = true
+    }
+  },
+  {
+    name = "TelescopePreviewNormal",
+    opts = { link = "Normal" }
+  },
+  -- results
+  {
+    name = "TelescopeResultsTitle",
+    opts = {
+      bg = get_fg("TelescopeResultsTitle"),
+      fg = get_bg("TelescopeNormal"),
+      bold = true
+    }
+  },
+  {
+    name = "TelescopeResultsNormal",
+    opts = { link = "CursorLine" }
+  },
+  {
+    name = "TelescopeSelection",
+    opts = { bg = get_bg("TelescopeNormal") }
+  },
+  {
+    name = "TelescopeSelectionCaret",
+    opts = {
+      bg = get_bg("TelescopeNormal"),
+      fg = get_fg("TelescopeSelectionCaret"),
+      bold = true
+    }
+  },
+}
 
-local set_hl = vim.api.nvim_set_hl
+for _, hl in ipairs(hightlights) do
+  if not hl.name then
+    vim.notify("Highlight has no name!", vim.log.levels.ERROR)
+  end
+  if not hl.opts then
+    vim.notify("Highlight " .. hl.name .. " has no options!", vim.log.levels.ERROR)
+  end
+  local set_hl = vim.api.nvim_set_hl
 
----- Telescope
--- borders
-set_hl(0, "TelescopePromptBorder", { link = "CursorLine" })
-set_hl(0, "TelescopePreviewBorder", { link = "CursorLine" })
-set_hl(0, "TelescopeResultsBorder", { link = "CursorLine" })
--- prompt
-set_hl(0, "TelescopePromptTitle",
-  { bg = get_fg("TelescopePromptTitle"), fg = get_bg("TelescopeNormal"), bold = true })
-set_hl(0, "TelescopePromptPrefix",
-  { fg = get_fg("TelescopePromptPrefix"), bg = get_bg("CursorLine"), bold = true })
-set_hl(0, "TelescopePromptNormal", { link = "CursorLine" }) -- for some reason this does not affect the prompt text?
--- preview
-set_hl(0, "TelescopePreviewTitle",
-  { bg = get_fg("TelescopePreviewTitle"), fg = get_bg("TelescopeNormal"), bold = true })
-set_hl(0, "TelescopePreviewNormal", { link = "Normal" })
--- results
-set_hl(0, "TelescopeResultsTitle",
-  { bg = get_fg("TelescopeResultsTitle"), fg = get_bg("TelescopeNormal"), bold = true })
-set_hl(0, "TelescopeResultsNormal",
-  { link = "CursorLine" })
-set_hl(0, "TelescopeSelection", { bg = get_bg("TelescopeNormal") })
-set_hl(0, "TelescopeSelectionCaret",
-  { bg = get_bg("TelescopeNormal"), fg = get_fg("TelescopeSelectionCaret"), bold = true })
-----
+  set_hl(0, hl.name,
+    { fg = hl.opts.fg, bg = hl.opts.bg, link = hl.opts.link, bold = hl.opts.bold, italic = hl.opts.italic })
+end
