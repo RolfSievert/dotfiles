ca4ab4ff430d4402fe7e04bb7a93367ac46b2024
rolfsievert <26115145+RolfSievert@users.noreply.github.com>
2026-01-31 13:33:34 +0100

    changed doc scroll in nvim to step 1 line instead of 4

--- home/.config/nvim/lua/plugin_configs/cmp_setup.lua
+++ home/.config/nvim/lua/plugin_configs/cmp_setup.lua
@@ -17,8 +17,8 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ['<Tab>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
     ['<S-Tab>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-    ['<C-k>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-j>'] = cmp.mapping.scroll_docs(4),
+    ['<C-k>'] = cmp.mapping.scroll_docs(-1),
+    ['<C-j>'] = cmp.mapping.scroll_docs(1),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
