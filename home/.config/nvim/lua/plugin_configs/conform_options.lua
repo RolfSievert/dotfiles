f1e2e6855873c98ddd0a54a9b41e36c572c99e29
Rolf Sievert <26115145+RolfSievert@users.noreply.github.com>
2026-03-01 11:05:42 +0100

    fixed python formatting/fix

--- home/.config/nvim/lua/plugin_configs/conform_options.lua
+++ home/.config/nvim/lua/plugin_configs/conform_options.lua
@@ -1,6 +1,8 @@
 local options = {
+  -- NOTE: lsp formatting is always used as fallback, see keymapping configurations
   formatters_by_ft = {
-    cs = { "csharpier" }
+    cs = { "csharpier" },
+    python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
   },
 }
 
