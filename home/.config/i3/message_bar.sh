02873d91404a787a54bc7cf7f2387574d8bd54cc
Rolf Sievert <26115145+RolfSievert@users.noreply.github.com>
2026-03-06 21:00:40 +0100

    fixed warnings fixed outdated dunst argument

--- home/.config/i3/message_bar.sh
+++ home/.config/i3/message_bar.sh
@@ -6,20 +6,18 @@ function send_notification_message_bar {
     msgId="$3"
     # Calculate number of dashes and spaces
     barsize=5
-    dashes=$(($value / $barsize))
-    spaces=$(((100 / $barsize) - $dashes))
+    dashes=$((value / barsize))
+    spaces=$(((100 / barsize) - dashes))
     # Make the bar with special character ─ (not dash -)
     # NOTE: this creates at least one character
-    bar1=$(printf "█"'%.s' $(eval "echo {0.."$(($dashes))"}"))
+    bar1=$(printf "█"'%.s' $(eval "echo {0.."$((dashes))"}"))
     # Create spaces to make notification size constant
-    bar2=$(printf " "'%.s' $(eval "echo {0.."$(($spaces))"}"))
-
-    echo \"$bar2\"
+    bar2=$(printf " "'%.s' $(eval "echo {0.."$((spaces))"}"))
 
     # Center the title
-    title_indent_count=$((((100 / $barsize) - ${#title}) / 2))
-    title_indent=$(printf " "'%.s' $(eval "echo {0.."$(($title_indent_count))"}"))
+    title_indent_count=$((((100 / barsize) - ${#title}) / 2))
+    title_indent=$(printf " "'%.s' "$(eval "echo {0.."$((title_indent_count))"}")")
 
     # Send the notification, -r is int reference value
-    dunstify --appname=custom-bar -u normal --icon=0 -r "$msgId" "$title" "${bar2:1}$bar1"
+    dunstify --app-name=custom-bar -u normal --icon=0 -r "$msgId" "$title" "${bar2:1}$bar1"
 }
--- home/.config/i3/volume.sh
+++ home/.config/i3/volume.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Script usage:
 # $./volume.sh up vol
 # $./volume.sh down vol
 # $./volume.sh mute
 
-source $(dirname $0)/message_bar.sh
+source "$(dirname "$0")"/message_bar.sh
 
 # Arbitrary but unique message id
 msgId="991049"
@@ -20,7 +20,7 @@ function is_mute {
 }
 
 function get_default_sink {
-    echo "$(pactl get-default-sink)"
+    pactl get-default-sink
 }
 
 function get_device_name {
@@ -28,24 +28,27 @@ function get_device_name {
     # after that line (which is the device name)
     device_code=$(pactl get-default-sink | cut -f2 -d.)
     card_line_number=$(pactl list cards | grep -n "\.${device_code}" | cut -f1 -d: | head -1)
-    device_lines=($(pactl list cards | grep -n "device.description" | cut -f1 -d:))
+    device_lines=("$(pactl list cards | grep -n "device.description" | cut -f1 -d:)")
     for device_line in "${device_lines[@]}"; do
         if [ "$device_line" -gt "$card_line_number" ]; then
-            echo $(pactl list cards \
+            pactl list cards \
                 | sed "${device_line}q;d" \
                 | grep -o '".*"' \
-                | sed 's/"//g')
+                | sed 's/"//g'
         fi
     done
 }
 
-function send_notification {
-    volume=$(get_volume $1)
+function send_notification() {
+    echo send notification
+    volume=$(get_volume "$1")
+    echo vol $volume
 
     device_name=$(get_device_name)
+    echo $device_name
 
     # Set title
-    if [[ "$(is_mute $1)" == *"yes"* ]]; then
+    if [[ "$(is_mute "$1")" == *"yes"* ]]; then
         title="Muted (${device_name})"
     else
         title="Volume (${device_name})"
@@ -54,27 +57,27 @@ function send_notification {
     send_notification_message_bar "$volume" "$title" "$msgId"
 }
 
-sink=`get_default_sink`
+sink=$(get_default_sink)
 vdiff=$2 # Difference to change volume with
 
 case $1 in
     up)
         # Up the volume
-        volume=$(get_volume $sink)
+        volume=$(get_volume "$sink")
         if [[ "$volume" -ge 95 ]]; then
             pactl set-sink-volume "$sink" 100%
         else
-            pactl set-sink-volume "$sink" +$vdiff%
+            pactl set-sink-volume "$sink" +"$vdiff"%
         fi
-        send_notification $sink
+        send_notification "$sink"
         ;;
     down)
-        pactl set-sink-volume "$sink" -$vdiff%
-	    send_notification $sink
+        pactl set-sink-volume "$sink" -"$vdiff"%
+	    send_notification "$sink"
 	    ;;
     mute)
     	# Toggle mute
         pactl set-sink-mute "$sink" toggle
-        send_notification $sink
+        send_notification "$sink"
 	    ;;
 esac
